69a2d339298b605ab3525afc2fd7e7cbeeee832f
Yichi Zhang <zhang-yi-chi@hotmail.com>
2015-03-10 09:06:13 +1100

    Initial commit

---
+++ NumberKeyboardViewDemo/NumberKeyboardViewDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NumberKeyboardViewDemo
+//
+//  Created by Yichi on 10/03/2015.
+//  Copyright (c) 2015 Yichi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
