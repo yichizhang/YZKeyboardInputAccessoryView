1dde79b80ceb3dd19f5c543b70321a4cce91b105
Yichi Zhang <zhang-yi-chi@hotmail.com>
2015-03-12 17:34:15 +1100

    Added keyboard buttons

--- Source/YZNumberKeyboardView.swift
+++ Source/YZNumberKeyboardView.swift
@@ -10,7 +10,9 @@ import Foundation
 import UIKit
 
 class YZNumberKeyboardView : UIView, UIInputViewAudioFeedback {
-	weak var textInput:UITextInput?
+	weak var textField:UITextField?
+	weak var textView:UITextView?
+	var numberKeyButtons:[CYRKeyboardButton] = Array()
 	lazy var dissmissButton:UIButton = {
 		let b = UIButton.buttonWithType(.Custom) as UIButton
 		b.setTitle("Close", forState: .Normal)
@@ -23,15 +25,16 @@ class YZNumberKeyboardView : UIView, UIInputViewAudioFeedback {
 	
 	class func attachTo(#textInput:UITextInput) {
 		let view = YZNumberKeyboardView()
-		view.textInput = textInput
 		
 		if(textInput.isKindOfClass(UITextField)) {
 			var t = textInput as UITextField;
 			t.inputAccessoryView = view
+			view.textField = t
 		}
 		else if(textInput.isKindOfClass(UITextView)) {
 			var t = textInput as UITextView;
 			t.inputAccessoryView = view
+			view.textView = t
 		}
 	}
 	
@@ -39,12 +42,25 @@ class YZNumberKeyboardView : UIView, UIInputViewAudioFeedback {
 		self.frame = CGRect(x: 0, y: 0, width: 320, height: 60)
 		self.backgroundColor = UIColor.blueColor()
 		
-		self.addSubview(self.dissmissButton)
+		dissmissButton.addTarget(self, action: "dissmissButtonTapped:", forControlEvents: UIControlEvents.TouchUpInside)
+		addSubview(dissmissButton)
+		
+		for i in 1...10 {
+			let key = "\(i%10)"
+			let b = CYRKeyboardButton()
+			b.autoresizingMask = UIViewAutoresizing.FlexibleWidth | UIViewAutoresizing.FlexibleLeftMargin | UIViewAutoresizing.FlexibleRightMargin
+			b.addTarget(self, action: "numberKeyTapped:", forControlEvents: .TouchUpInside)
+			b.input = key
+			
+			numberKeyButtons.append(b)
+			addSubview(b)
+		}
+		
+		
 	}
 	
 	override init() {
 		super.init()
-		commonInit()
 	}
 	
 	override init(frame: CGRect) {
@@ -63,10 +79,37 @@ class YZNumberKeyboardView : UIView, UIInputViewAudioFeedback {
 		let size = self.dissmissButton.bounds.size
 		self.dissmissButton.frame = CGRect(
 			x: self.bounds.width - size.width,
-			y: -size.height,
+			y: -size.height*0.5,
 			width: size.width,
 			height: size.height
 		)
+		
+		let padding = CGFloat(4)
+		let keyCount = CGFloat(numberKeyButtons.count)
+		let keyWidth = (self.bounds.width - padding * (keyCount + 1)) / keyCount
+		
+		for (index, keyButton) in enumerate(numberKeyButtons) {
+			let i = CGFloat(index)
+			keyButton.frame = CGRect(
+				x: padding * (i+1.0) + keyWidth * i,
+				y: padding + 10,
+				width: keyWidth,
+				height: bounds.height - 2*padding - 10
+			)
+		}
+	}
+	
+	// MARK: Dissmiss button tapped.
+	func dissmissButtonTapped(sender:AnyObject) {
+		textField?.resignFirstResponder()
+		textView?.resignFirstResponder()
+	}
+	
+	// MARK: Number button tapped
+	func numberKeyTapped(sender:CYRKeyboardButton) {
+		UIDevice.currentDevice().playInputClick()
+		self.textView?.insertText(sender.input)
+		self.textField?.insertText(sender.input)
 	}
 	
 }
